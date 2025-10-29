23a3a6383df2c62ce8087d4ea1cbd89827d85f68
Flyxs <felypecarrijo19@gmail.com>
2025-10-29 15:43:19 -0300

    Adiciona função para remover valores específicos da lista e implementa a impressão da lista

--- Lista-encadeada/lista_encadeada.c
+++ Lista-encadeada/lista_encadeada.c
@@ -49,6 +49,16 @@ void remove_inicio(celula *le) {
     free(lixo);
 }
 
+// Removes todos os valores = x da lista
+void remove_valores(celula *le, int x) {
+    while (le -> prox != NULL) {
+        if (le -> prox -> dado == x) {
+            remove_inicio(le);
+        }
+        else le = le -> prox;
+    }
+}
+
 // Remove o final da lista
 void remove_final(celula *le) {
     if (le -> prox == NULL) {
@@ -70,4 +80,73 @@ void destroi_lista(celula *le) {
         remove_inicio(le);
     }
     free(le);
+}
+
+void imprimir_lista(celula *le) {
+    if (le == NULL) {
+        printf("A lista ainda não foi criada\n");
+        return;
+    }
+    printf("[");
+    celula *atual = le -> prox;
+    while (atual != NULL) {
+        printf(" %d ->", atual -> dado);
+        atual = atual -> prox;
+    }
+    printf(" NULL ]");
+}
+
+void limpar_terminal() {
+    #ifdef _WIN32
+        // Se for Windows
+        system("cls");
+    #else
+        // Se for Linux, macOS ou outro
+        system("clear");
+    #endif
+}
+
+int main() {
+    int true = 1;
+
+    celula *minhalista = NULL;
+
+    while (true == 1) {
+        limpar_terminal();
+
+        int escolha;
+        imprimir_lista(minhalista);
+        printf("Escolha uma opção:\n [1]Criar lista\n [2]Adicionar Valor\n [3]Remover Elemento\n [4]Apagar Lista\n [5]Sair\n");
+        scanf("%d", &escolha);
+        
+        if (escolha == 1) {
+            if (minhalista != NULL) {
+                printf("A lista já existe!\n");
+            }
+            else minhalista = cria_lista();
+        }
+
+        else if (escolha == 2) {
+            
+        }
+
+        else if (escolha == 3) {
+
+        }
+
+        else if (escolha == 4) {
+
+        }
+        
+        else if (escolha == 5) {
+            if (minhalista != NULL) {
+                destroi_lista(minhalista);
+                minhalista = NULL; 
+            }
+            printf("Saindo...\n");
+            true = 0; 
+        }
+
+        else printf("Comando Invalido\n");
+    }
 }
